898780f68d43988ef3d79e2db54c7e773fa05baa
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-09-09 18:07:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,8 +17,8 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   end
   if phrase != phrase.upcase()
-    
+    return "HUH?! SPEAK UP, SONNY!"
+  else 
+    return "NO, NOT SINCE 1938!"
   end
-  
 end
-speak_to_grandma
